f971e1efd9967b9d74f407da7ff9ffb01170aa5b
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-12-10 20:32:31 -0500

    Smart-Contract

--- contracts/Lw3Punks.sol
+++ contracts/Lw3Punks.sol
@@ -1,11 +1,28 @@
-//SPDX-Licence-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-pragma solidiy ^0.8.4;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract Lw3Punks is ERC721Enumerable Ownable{
+contract Lw3Punks is ERC721Enumerable, Ownable {
+using Strings for uint256;
 
+string _baseTokenURI;
+
+uint256 public _price = 0.01 ether;
+
+bool public _paused;
+
+uint256 public _maxTokenIds = 10;
+
+uint256 public tokenIds;
+
+modifier onlyWhenNotPaused {
+    require(!_paused, "Contract currently paused");
+    _;
+}
+
+constructor {}
 }
